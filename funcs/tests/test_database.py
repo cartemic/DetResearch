dc8b2b87b1e151e0246d50b32180dac32b66c83a
Mick Carter <cartemic@oregonstate.edu>
2019-04-29 13:42:26 -0700

    changed tests to accommodate three-table method of data storage

--- funcs/tests/test_database.py
+++ funcs/tests/test_database.py
@@ -7,6 +7,7 @@ import sqlite3
 import pytest
 import inspect
 from numpy import isclose, allclose
+import uuid
 
 
 def remove_stragglers():
@@ -67,21 +68,6 @@ class TestDataBase:
 
 # noinspection PyProtectedMember,PyUnresolvedReferences
 class TestTable:
-    keys = {
-        'date_stored',
-        'mechanism',
-        'initial_temp',
-        'initial_press',
-        'equivalence',
-        'fuel',
-        'oxidizer',
-        'diluent',
-        'diluent_mol_frac',
-        'reaction_number',
-        'k_i',
-        'cj_speed'
-    }
-
     # noinspection PyProtectedMember,PyUnresolvedReferences
     class FakeTable:
         """
@@ -96,10 +82,11 @@ class TestTable:
             self.database = test_db
             self.table_name = test_table_name
             self.con = sqlite3.connect(self.database)
+            self._build_query_str = db.Table._build_query_str
             if allow_create and self.table_name not in\
                     db.DataBase.list_all_tables(test_db):
                 bind(self, db.Table._create_test_table)
-                self._create()
+                self._create_test_table()
 
         def __del__(self):
             self.con.commit()
@@ -144,14 +131,14 @@ class TestTable:
             {**base_inputs, 'initial_temp': 300},
             {**base_inputs, 'initial_temp': 300, 'fuel': 'CH4'}
         ]
-        test_results = [db.Table._build_query_str(**kw) for kw in inputs]
+        test_results = [db.Table._build_query_str(kw) for kw in inputs]
         # dict order is unreliable; use sorted() to compare strings
         assert all([
             ''.join(sorted(test)) == ''.join(sorted(good))
             for test, good in zip(test_results, good_results)
         ])
 
-    def test_columns(self):
+    def test_test_columns(self):
         num_cols = 16
         num_chars = 16
         test_db = generate_db_name()
@@ -177,49 +164,160 @@ class TestTable:
             test_table_name,
             allow_create=False
             )
-        bind(test_table, db.Table.columns)
-        test_columns = test_table.columns()
+        bind(test_table, db.Table.test_columns)
+        test_columns = test_table.test_columns()
         assert all([
             test == good for test, good in zip(test_columns, column_names)
         ])
 
-    def test__create(self):
+    def test_base_columns(self):
+        num_cols = 16
+        num_chars = 16
+        test_db = generate_db_name()
+        column_names = [
+            ''.join([random.choice(string.ascii_letters)
+                     for _ in range(num_chars)])
+            for _ in range(num_cols)
+        ]
+        test_table_name = 'test_table'
+        header = 'CREATE TABLE {:s} ('.format(test_table_name)
+        footer = ' TEXT);'
+        sep = ' TEXT, '
+
+        with sqlite3.connect(test_db) as con:
+            cur = con.cursor()
+            cur.execute(
+                header + sep.join(column_names) + footer
+            )
+        con.close()
+
+        test_table = self.FakeTable(
+            test_db,
+            test_table_name,
+            allow_create=False
+            )
+        bind(test_table, db.Table.base_columns)
+        test_table._rxn_table_id = str(uuid.uuid4()).replace('-', '')
+        test_columns = test_table.base_columns()
+        assert all([
+            test == good for test, good in zip(test_columns, column_names)
+        ])
+
+    def test_pert_columns(self):
+        num_cols = 16
+        num_chars = 16
+        test_db = generate_db_name()
+        column_names = [
+            ''.join([random.choice(string.ascii_letters)
+                     for _ in range(num_chars)])
+            for _ in range(num_cols)
+        ]
+        test_table_name = 'test_table'
+        header = 'CREATE TABLE {:s} ('.format(test_table_name)
+        footer = ' TEXT);'
+        sep = ' TEXT, '
+
+        with sqlite3.connect(test_db) as con:
+            cur = con.cursor()
+            cur.execute(
+                header + sep.join(column_names) + footer
+            )
+        con.close()
+
+        test_table = self.FakeTable(
+            test_db,
+            test_table_name,
+            allow_create=False
+            )
+        bind(test_table, db.Table.pert_columns)
+        test_table._rxn_table_id = str(uuid.uuid4()).replace('-', '')
+        test_columns = test_table.pert_columns()
+        assert all([
+            test == good for test, good in zip(test_columns, column_names)
+        ])
+
+    def test__create_test_table(self):
         test_db = generate_db_name()
         test_table_name = 'test_table'
         FakeTable = self.FakeTable
-        FakeTable.columns = db.Table.columns
+        FakeTable.columns = db.Table.test_columns
         test_table = FakeTable(
             test_db,
             test_table_name,
             allow_create=True
             )
         actual_columns = set(test_table.columns())
-        assert not self.keys.difference(actual_columns)
+        assert not set(db.Table._test_table_args).difference(actual_columns)
 
-    def test_store_and_check_existing(self):
+    def test__create_base_table(self):
+        test_db = generate_db_name()
+        test_table_name = 'test_table'
+        test_table = db.Table(
+            test_db,
+            test_table_name,
+            testing=True
+            )
+        actual_columns = set(test_table.base_columns())
+        assert not {'rxn_no', 'rxn', 'k_i'}.difference(actual_columns)
+
+    def test__create_pert_table(self):
+        test_db = generate_db_name()
+        test_table_name = 'test_table'
+        test_table = db.Table(
+            test_db,
+            test_table_name,
+            testing=True
+            )
+        actual_columns = set(test_table.pert_columns())
+        assert not {
+            'rxn_no',
+            'rxn',
+            'k_i',
+            'cj_speed',
+            'ind_len_west',
+            'ind_len_gav',
+            'ind_len_ng',
+            'cell_size_west',
+            'cell_size_gav',
+            'cell_size_ng',
+            'sens_cj_speed',
+            'sens_ind_len_west',
+            'sens_ind_len_gav',
+            'sens_ind_len_ng',
+            'sens_cell_size_west',
+            'sens_cell_size_gav',
+            'sens_cell_size_ng',
+        }.difference(actual_columns)
+
+    def test_store_and_check_existing_test(self):
         kwargs = {
             'mechanism': 'gri30.cti',
             'initial_temp': 300,
             'initial_press': 101325,
-            'equivalence': 1,
             'fuel': 'CH4',
             'oxidizer': 'N2O',
-            'reaction_number': 0,
+            'equivalence': 1,
             'diluent': 'N2',
             'diluent_mol_frac': 0.1
         }
         test_db = generate_db_name()
         test_table_name = 'test_table'
-        FakeTable = self.FakeTable
-        test_table = FakeTable(
+        test_table = db.Table(
             test_db,
             test_table_name,
-            allow_create=True
+            testing=True
         )
-        bind(test_table, db.Table.store_row)
-        bind(test_table, db.Table.check_existing_test)
-        test_table.store_row(**{**kwargs, 'k_i': 123, 'cj_speed': 456})
-        assert test_table.check_existing_row(**kwargs)
+        test_table.store_test_row(**{
+            **kwargs,
+            'cj_speed': 456,
+            'ind_len_west': 1,
+            'ind_len_gav': 2,
+            'ind_len_ng': 3,
+            'cell_size_west': 4,
+            'cell_size_gav': 5,
+            'cell_size_ng': 6
+        })
+        assert test_table.check_existing_test(**kwargs)
 
     def test_fetch_rows_blank_table(self):
         test_db = generate_db_name()
@@ -229,9 +327,10 @@ class TestTable:
             test_table_name,
             testing=True
             )
-        test_rows = test_table.fetch_rows()
+        test_rows = test_table.fetch_test_rows()
         assert all([
-            not self.keys.difference(set(test_rows.keys())),
+            not set(db.Table._test_table_args)
+                .difference(set(test_rows.keys())),
             *[item == [] for item in test_rows.values()]
         ])
 
@@ -247,20 +346,24 @@ class TestTable:
             'mechanism': 'gri30.cti',
             'initial_temp': 300,
             'initial_press': 101325,
-            'equivalence': 1,
             'fuel': 'CH4',
             'oxidizer': 'N2O',
-            'reaction_number': 0,
+            'equivalence': 1,
             'diluent': 'N2',
             'diluent_mol_frac': 0.1,
-            'k_i': 1.2e12,
-            'cj_speed': 1986.12354679687543
+            'cj_speed': 1986.12354679687543,
+            'ind_len_west': 1,
+            'ind_len_gav': 2,
+            'ind_len_ng': 3,
+            'cell_size_west': 4,
+            'cell_size_gav': 5,
+            'cell_size_ng': 6
         }
-        test_table.store_row(**kwargs)
-        test_rows = test_table.fetch_rows()
+        test_table.store_test_row(**kwargs)
+        test_rows = test_table.fetch_test_rows()
         checks = []
         for key, value in test_rows.items():
-            if 'date' not in key:
+            if 'date' not in key and 'rxn_table_id' not in key:
                 if isinstance(kwargs[key], str) or isinstance(kwargs[key], int):
                     checks.append(kwargs[key] == value[0])
                 else:
@@ -279,36 +382,44 @@ class TestTable:
             'mechanism': 'gri30.cti',
             'initial_temp': 300,
             'initial_press': 101325,
-            'equivalence': 1,
             'fuel': 'CH4',
             'oxidizer': 'N2O',
-            'reaction_number': 0,
+            'equivalence': 1,
             'diluent': 'N2',
             'diluent_mol_frac': 0.1,
-            'k_i': 1.2e12,
-            'cj_speed': 1986.12354679687543
+            'cj_speed': 1986.12354679687543,
+            'ind_len_west': 1,
+            'ind_len_gav': 2,
+            'ind_len_ng': 3,
+            'cell_size_west': 4,
+            'cell_size_gav': 5,
+            'cell_size_ng': 6
         }, {
-            'mechanism': 'gri30.cti',
-            'initial_temp': 900,
-            'initial_press': 202156,
-            'equivalence': 2,
-            'fuel': 'C3H8',
-            'oxidizer': 'O2:1 N2:3.76',
-            'reaction_number': 12,
+            'mechanism': 'Mevel2017.cti',
+            'initial_temp': 550,
+            'initial_press': 2*101325,
+            'fuel': 'H2',
+            'oxidizer': 'O2',
+            'equivalence': 1.125,
             'diluent': 'None',
             'diluent_mol_frac': 0,
-            'k_i': 1.34e12,
-            'cj_speed': 1467.546546539687543
+            'cj_speed': 2112,
+            'ind_len_west': 2.1,
+            'ind_len_gav': 2.2,
+            'ind_len_ng': 2.3,
+            'cell_size_west': 2.4,
+            'cell_size_gav': 2.5,
+            'cell_size_ng': 2.6
         }]
         for kw in kwargs:
-            test_table.store_row(**kw)
-        test_rows = test_table.fetch_rows()
+            test_table.store_test_row(**kw)
+        test_rows = test_table.fetch_test_rows()
         checks = []
         good_answer = {
             key: [value, kwargs[1][key]] for key, value in kwargs[0].items()
         }
         for key, value in test_rows.items():
-            if 'date' not in key:
+            if 'date' not in key and 'rxn_table_id' not in key:
                 if (isinstance(kwargs[0][key], str)
                         or isinstance(kwargs[0][key], int)):
                     for good, test in zip(good_answer[key], test_rows[key]):
@@ -329,34 +440,42 @@ class TestTable:
             'mechanism': 'gri30.cti',
             'initial_temp': 300,
             'initial_press': 101325,
-            'equivalence': 1,
             'fuel': 'CH4',
             'oxidizer': 'N2O',
-            'reaction_number': 0,
+            'equivalence': 1,
             'diluent': 'N2',
             'diluent_mol_frac': 0.1,
-            'k_i': 1.2e12,
-            'cj_speed': 1986.12354679687543
+            'cj_speed': 1986.12354679687543,
+            'ind_len_west': 1,
+            'ind_len_gav': 2,
+            'ind_len_ng': 3,
+            'cell_size_west': 4,
+            'cell_size_gav': 5,
+            'cell_size_ng': 6
         }
         kwargs_repl = {
             'mechanism': 'gri30.cti',
             'initial_temp': 300,
             'initial_press': 101325,
-            'equivalence': 1,
             'fuel': 'CH4',
             'oxidizer': 'N2O',
-            'reaction_number': 0,
+            'equivalence': 1,
             'diluent': 'N2',
             'diluent_mol_frac': 0.1,
-            'k_i': 1.34e12,
-            'cj_speed': 1467.546546539687543
+            'cj_speed': 2112,
+            'ind_len_west': 2.1,
+            'ind_len_gav': 2.2,
+            'ind_len_ng': 2.3,
+            'cell_size_west': 2.4,
+            'cell_size_gav': 2.5,
+            'cell_size_ng': 2.6
         }
-        test_table.store_row(**kwargs_init)
-        test_table._update_row(**kwargs_repl)
-        test_rows = test_table.fetch_rows()
+        test_table.store_test_row(**kwargs_init)
+        test_table._update_test_row(**kwargs_repl)
+        test_rows = test_table.fetch_test_rows()
         checks = []
         for key, value in test_rows.items():
-            if 'date' not in key:
+            if 'date' not in key and 'rxn_table_id' not in key:
                 if (isinstance(kwargs_repl[key], str)
                         or isinstance(kwargs_repl[key], int)):
                     checks.append(kwargs_repl[key] == value[0])
@@ -376,34 +495,42 @@ class TestTable:
             'mechanism': 'gri30.cti',
             'initial_temp': 300,
             'initial_press': 101325,
-            'equivalence': 1,
             'fuel': 'CH4',
             'oxidizer': 'N2O',
-            'reaction_number': 0,
+            'equivalence': 1,
             'diluent': 'N2',
             'diluent_mol_frac': 0.1,
-            'k_i': 1.2e12,
-            'cj_speed': 1986.12354679687543
+            'cj_speed': 1986.12354679687543,
+            'ind_len_west': 1,
+            'ind_len_gav': 2,
+            'ind_len_ng': 3,
+            'cell_size_west': 4,
+            'cell_size_gav': 5,
+            'cell_size_ng': 6
         }
         kwargs_repl = {
             'mechanism': 'gri30.cti',
             'initial_temp': 300,
             'initial_press': 101325,
-            'equivalence': 1,
             'fuel': 'CH4',
             'oxidizer': 'N2O',
-            'reaction_number': 0,
+            'equivalence': 1,
             'diluent': 'N2',
             'diluent_mol_frac': 0.1,
-            'k_i': 1.34e12,
-            'cj_speed': 1467.546546539687543
+            'cj_speed': 2112,
+            'ind_len_west': 2.1,
+            'ind_len_gav': 2.2,
+            'ind_len_ng': 2.3,
+            'cell_size_west': 2.4,
+            'cell_size_gav': 2.5,
+            'cell_size_ng': 2.6
         }
-        test_table.store_row(**kwargs_init)
-        test_table.store_row(**kwargs_repl, overwrite_existing=True)
-        test_rows = test_table.fetch_rows()
+        test_table.store_test_row(**kwargs_init)
+        test_table.store_test_row(**kwargs_repl, overwrite_existing=True)
+        test_rows = test_table.fetch_test_rows()
         checks = []
         for key, value in test_rows.items():
-            if 'date' not in key:
+            if 'date' not in key and 'rxn_table_id' not in key:
                 if (isinstance(kwargs_repl[key], str)
                         or isinstance(kwargs_repl[key], int)):
                     checks.append(kwargs_repl[key] == value[0])
@@ -424,35 +551,43 @@ class TestTable:
             'mechanism': 'gri30.cti',
             'initial_temp': 300,
             'initial_press': 101325,
-            'equivalence': 1,
             'fuel': 'CH4',
             'oxidizer': 'N2O',
-            'reaction_number': 0,
+            'equivalence': 1,
             'diluent': 'N2',
             'diluent_mol_frac': 0.1,
-            'k_i': 1.2e12,
-            'cj_speed': 1986.12354679687543
+            'cj_speed': 1986.12354679687543,
+            'ind_len_west': 1.23354,
+            'ind_len_gav': 2.12354,
+            'ind_len_ng':  1.21354,
+            'cell_size_west': 25.354,
+            'cell_size_gav': 235.243254,
+            'cell_size_ng': .4874341,
         }
         kwargs_repl = {
             'mechanism': 'gri30.cti',
             'initial_temp': 300,
             'initial_press': 101325,
-            'equivalence': 1,
             'fuel': 'CH4',
             'oxidizer': 'N2O',
-            'reaction_number': 0,
+            'equivalence': 1,
             'diluent': 'N2',
             'diluent_mol_frac': 0.1,
-            'k_i': 1.34e12,
-            'cj_speed': 1467.546546539687543
+            'cj_speed': 1467.546546539687543,
+            'ind_len_west': 21.23354,
+            'ind_len_gav': 22.12354,
+            'ind_len_ng':  14.21354,
+            'cell_size_west': 265.354,
+            'cell_size_gav': 235.254643254,
+            'cell_size_ng': 76.4874341,
         }
-        test_table.store_row(**kwargs_init)
-        test_table.store_row(**kwargs_repl, overwrite_existing=False)
-        test_rows = test_table.fetch_rows()
+        test_table.store_test_row(**kwargs_init)
+        test_table.store_test_row(**kwargs_repl, overwrite_existing=False)
+        test_rows = test_table.fetch_test_rows()
 
         checks = []
         for key, value in test_rows.items():
-            if 'date' not in key:
+            if 'date' not in key and 'rxn_table_id' not in key:
                 if (isinstance(kwargs_init[key], str)
                         or isinstance(kwargs_init[key], int)):
                     checks.append(kwargs_init[key] == value[0])
