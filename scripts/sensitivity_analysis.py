428d930ff9aa9acba168fb784854695bc7baac17
Mick Carter <cartemic@oregonstate.edu>
2019-05-15 15:28:17 -0700

    updated analysis procedure to reflect lack of cj speed variation with perturbation due to the use of the equilibrate function to calculate cj speed (k_equil = k_f/k_r meaning that the perturbation divides out).

--- scripts/sensitivity_analysis.py
+++ scripts/sensitivity_analysis.py
@@ -33,6 +33,39 @@ def check_stored_base_cj_speed(
     return len(current_data['cj_speed']) == 1
 
 
+def check_stored_base_calcs(
+        table,
+        mechanism,
+        initial_temp,
+        initial_press,
+        fuel,
+        oxidizer,
+        equivalence,
+        diluent,
+        diluent_mol_frac,
+        inert,
+):
+    current_data = table.fetch_test_rows(
+            mechanism=mechanism,
+            initial_temp=initial_temp,
+            initial_press=initial_press,
+            fuel=fuel,
+            oxidizer=oxidizer,
+            equivalence=equivalence,
+            diluent=diluent,
+            diluent_mol_frac=diluent_mol_frac,
+            inert=inert
+    )
+    return all([
+        current_data['ind_len_west'][0] > 0,
+        current_data['ind_len_gav'][0] > 0,
+        current_data['ind_len_ng'][0] > 0,
+        current_data['cell_size_west'][0] > 0,
+        current_data['cell_size_gav'][0] > 0,
+        current_data['cell_size_ng'][0] > 0,
+    ])
+
+
 def perform_study(
         mech,
         init_temp,
@@ -43,10 +76,10 @@ def perform_study(
         diluent,
         diluent_mol_frac,
         inert,
-        # db_lock,
         perturbation_fraction,
         perturbed_reaction_no,
 ):
+    CellSize = cell_size.CellSize()
     gas = ct.Solution(mech)
     gas.TP = init_temp, init_press
     gas.set_equivalence_ratio(
@@ -62,49 +95,10 @@ def perform_study(
 
     )
 
-    # check for stored cj speed
-    stored = check_stored_base_cj_speed(
-        table=current_table,
-        mechanism=mech,
-        initial_temp=init_temp,
-        initial_press=init_press,
-        fuel=fuel,
-        oxidizer=oxidizer,
-        equivalence=equivalence,
-        diluent=diluent,
-        diluent_mol_frac=diluent_mol_frac,
-        inert=inert
-    )
-    if not stored:
-        # calculate and store cj speed
-        base_cj_speed = sd.postshock.CJspeed(
-            P1=init_press,
-            T1=init_temp,
-            q=gas.mole_fraction_dict(),
-            mech=mech
-        )
-        with db_lock:
-            rxn_table_id = current_table.store_test_row(
-                mechanism=mech,
-                initial_temp=init_temp,
-                initial_press=init_press,
-                fuel=fuel,
-                oxidizer=oxidizer,
-                equivalence=equivalence,
-                diluent=diluent,
-                diluent_mol_frac=diluent_mol_frac,
-                inert=inert,
-                cj_speed=base_cj_speed,
-                ind_len_west=0,
-                ind_len_gav=0,
-                ind_len_ng=0,
-                cell_size_west=0,
-                cell_size_gav=0,
-                cell_size_ng=0,
-                overwrite_existing=False
-            )
-    else:
-        current_data = current_table.fetch_test_rows(
+    with db_lock:
+        # check for stored cj speed
+        stored_cj = check_stored_base_cj_speed(
+            table=current_table,
             mechanism=mech,
             initial_temp=init_temp,
             initial_press=init_press,
@@ -115,40 +109,62 @@ def perform_study(
             diluent_mol_frac=diluent_mol_frac,
             inert=inert
         )
-        [rxn_table_id] = current_data['rxn_table_id']
-        [base_cj_speed] = current_data['cj_speed']
-        del current_data
+        if not stored_cj:
+            # calculate and store cj speed
+            cj_speed = sd.postshock.CJspeed(
+                P1=init_press,
+                T1=init_temp,
+                q=gas.mole_fraction_dict(),
+                mech=mech
+            )
+            rxn_table_id = current_table.store_test_row(
+                mechanism=mech,
+                initial_temp=init_temp,
+                initial_press=init_press,
+                fuel=fuel,
+                oxidizer=oxidizer,
+                equivalence=equivalence,
+                diluent=diluent,
+                diluent_mol_frac=diluent_mol_frac,
+                inert=inert,
+                cj_speed=cj_speed,
+                ind_len_west=0,
+                ind_len_gav=0,
+                ind_len_ng=0,
+                cell_size_west=0,
+                cell_size_gav=0,
+                cell_size_ng=0,
+                overwrite_existing=False
+            )
+        else:
+            # stored cj speed exists
+            current_data = current_table.fetch_test_rows(
+                mechanism=mech,
+                initial_temp=init_temp,
+                initial_press=init_press,
+                fuel=fuel,
+                oxidizer=oxidizer,
+                equivalence=equivalence,
+                diluent=diluent,
+                diluent_mol_frac=diluent_mol_frac,
+                inert=inert
+            )
+            [rxn_table_id] = current_data['rxn_table_id']
+            [cj_speed] = current_data['cj_speed']
+            del current_data
+        del stored_cj
 
-    # check for stored base reaction data
-    stored = current_table.check_for_stored_base_data(rxn_table_id)
-    if not stored:
-        with db_lock:
+        # check for stored base reaction data
+        stored_rxns = current_table.check_for_stored_base_data(rxn_table_id)
+        if not stored_rxns:
             current_table.store_base_rxn_table(
                 rxn_table_id=rxn_table_id,
                 gas=gas
-            )
-    del stored
+                )
+        del stored_rxns
 
-    # calculate base cell size
-    CellSize = cell_size.CellSize()
-    base_cell_calcs = CellSize(
-        base_mechanism=mech,
-        cj_speed=base_cj_speed,
-        initial_temp=init_temp,
-        initial_press=init_press,
-        fuel=fuel,
-        oxidizer=oxidizer,
-        equivalence=equivalence,
-        diluent=diluent,
-        diluent_mol_frac=diluent_mol_frac,
-        inert=inert,
-        database=db_name,
-        table_name=table_name
-    )
-    base_ind_len = CellSize.induction_length
-
-    with db_lock:
-        current_table.store_test_row(
+        stored_base_calcs = check_stored_base_calcs(
+            table=current_table,
             mechanism=mech,
             initial_temp=init_temp,
             initial_press=init_press,
@@ -157,16 +173,69 @@ def perform_study(
             equivalence=equivalence,
             diluent=diluent,
             diluent_mol_frac=diluent_mol_frac,
-            inert=inert,
-            cj_speed=base_cj_speed,
-            ind_len_west=base_ind_len['Westbrook'],
-            ind_len_gav=base_ind_len['Gavrikov'],
-            ind_len_ng=base_ind_len['Ng'],
-            cell_size_west=base_cell_calcs['Westbrook'],
-            cell_size_gav=base_cell_calcs['Gavrikov'],
-            cell_size_ng=base_cell_calcs['Ng'],
-            overwrite_existing=True
+            inert=inert
         )
+        if not stored_base_calcs:
+            # calculate base cell size
+            base_cell_calcs = CellSize(
+                base_mechanism=mech,
+                cj_speed=cj_speed,
+                initial_temp=init_temp,
+                initial_press=init_press,
+                fuel=fuel,
+                oxidizer=oxidizer,
+                equivalence=equivalence,
+                diluent=diluent,
+                diluent_mol_frac=diluent_mol_frac,
+                inert=inert,
+                database=db_name,
+                table_name=table_name
+            )
+            base_ind_len = CellSize.induction_length
+            current_table.store_test_row(
+                mechanism=mech,
+                initial_temp=init_temp,
+                initial_press=init_press,
+                fuel=fuel,
+                oxidizer=oxidizer,
+                equivalence=equivalence,
+                diluent=diluent,
+                diluent_mol_frac=diluent_mol_frac,
+                inert=inert,
+                cj_speed=cj_speed,
+                ind_len_west=base_ind_len['Westbrook'],
+                ind_len_gav=base_ind_len['Gavrikov'],
+                ind_len_ng=base_ind_len['Ng'],
+                cell_size_west=base_cell_calcs['Westbrook'],
+                cell_size_gav=base_cell_calcs['Gavrikov'],
+                cell_size_ng=base_cell_calcs['Ng'],
+                overwrite_existing=True
+            )
+        else:
+            # look up base calcs from db
+            current_data = current_table.fetch_test_rows(
+                mechanism=mech,
+                initial_temp=init_temp,
+                initial_press=init_press,
+                fuel=fuel,
+                oxidizer=oxidizer,
+                equivalence=equivalence,
+                diluent=diluent,
+                diluent_mol_frac=diluent_mol_frac,
+                inert=inert
+            )
+            base_ind_len = {
+                'Westbrook': current_data['ind_len_west'][0],
+                'Gavrikov': current_data['ind_len_gav'][0],
+                'Ng': current_data['ind_len_ng'][0],
+            }
+            base_cell_calcs = {
+                'Westbrook': current_data['cell_size_west'][0],
+                'Gavrikov': current_data['cell_size_gav'][0],
+                'Ng': current_data['cell_size_ng'][0],
+            }
+            del current_data
+        del stored_base_calcs
 
     # perturb gas (set multiplier)
     if perturbed_reaction_no != -1:
@@ -182,27 +251,10 @@ def perform_study(
 
         sd.postshock.ct.Solution = my_solution
 
-    # check for existing perturbed cj speed
-    current_data = current_table.fetch_pert_table(
-        rxn_table_id=rxn_table_id,
-        rxn_no=perturbed_reaction_no,
-    )
-    if len(current_data['cj_speed']) == 0:
-        # calculate cj speed
-        pert_cj_speed = sd.postshock.CJspeed(
-            P1=init_press,
-            T1=init_temp,
-            q=gas.mole_fraction_dict(),
-            mech=mech
-        )
-    else:
-        [pert_cj_speed] = current_data['cj_speed']
-    del current_data
-
     # calculate perturbed cell size
     pert_cell_calcs = CellSize(
         base_mechanism=mech,
-        cj_speed=pert_cj_speed,
+        cj_speed=cj_speed,
         initial_temp=init_temp,
         initial_press=init_press,
         fuel=fuel,
@@ -218,8 +270,6 @@ def perform_study(
     pert_ind_len = CellSize.induction_length
 
     # calculate sensitivities
-    sens_cj_speed = (pert_cj_speed - base_cj_speed) / \
-                    (base_cj_speed * perturbation_fraction)
     sens_ind_len_west = (pert_ind_len['Westbrook'] -
                          base_ind_len['Westbrook']) / \
                         (base_ind_len['Westbrook'] * perturbation_fraction)
@@ -245,14 +295,12 @@ def perform_study(
             rxn_no=perturbed_reaction_no,
             rxn=gas.reaction_equation(perturbed_reaction_no),
             k_i=gas.forward_rate_constants[perturbed_reaction_no],
-            cj_speed=pert_cj_speed,
             ind_len_west=pert_ind_len['Westbrook'],
             ind_len_gav=pert_ind_len['Gavrikov'],
             ind_len_ng=pert_ind_len['Ng'],
             cell_size_west=pert_cell_calcs['Westbrook'],
             cell_size_gav=pert_cell_calcs['Gavrikov'],
             cell_size_ng=pert_cell_calcs['Ng'],
-            sens_cj_speed=sens_cj_speed,
             sens_ind_len_west=sens_ind_len_west,
             sens_ind_len_gav=sens_ind_len_gav,
             sens_ind_len_ng=sens_ind_len_ng,
