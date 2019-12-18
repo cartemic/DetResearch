b8f2929182b292a4f017034e561e44ba3e941838
Mick Carter <cartemic@oregonstate.edu>
2019-12-18 04:02:59 -0800

    Update setup.py
    Add new requirements for post processing tools

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 from setuptools import setup, find_packages
 
-setup(name='funcs', package=find_packages())
+setup(name='funcs', package=find_packages(), install_requires=['pandas',
+                                                               'nptdms'])
