35a2444527ff350eaecb4be3ec303c2447301f8d
Mick Carter <cartemic@oregonstate.edu>
2020-03-02 20:13:30 -0800

    Add option to apply uncertainty to pixel deltas

--- funcs/soot_foil.py
+++ funcs/soot_foil.py
@@ -1,10 +1,15 @@
 # third party imports
 import numpy as np
 from skimage import io
+from uncertainties import unumpy as unp
+
+# local imports
+from . import uncertainty as un
 
 
 def get_px_deltas_from_lines(
         img_path,
+        apply_uncertainty=True
 ):
     """
     Returns an array of per-row triple point deltas (in pixels) from a given
@@ -16,10 +21,13 @@ def get_px_deltas_from_lines(
     ----------
     img_path : str
         path of the image containing traced triple point lines
+    apply_uncertainty : bool
+        True returns array of nominal values with uncertainties; False returns
+        only nominal values
 
     Returns
     -------
-    deltas : np.array
+    deltas : np.array or unp.uarray
         triple point distances, in pixels.
     """
     img = io.imread(img_path)
@@ -28,4 +36,15 @@ def get_px_deltas_from_lines(
     for row in range(img.shape[0]):
         diffs = np.diff(np.where(img[row] == img_max)[0])
         deltas = np.append(deltas, diffs)
+
+    if apply_uncertainty:
+        uncert = un.add_uncertainty_terms([
+            un.u_cell["soot_foil"]["delta_px"]["b"],
+            un.u_cell["soot_foil"]["delta_px"]["p"]
+        ])
+        deltas = unp.uarray(
+            deltas,
+            uncert
+        )
+
     return deltas
