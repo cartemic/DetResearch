6f417ce6c9e1324ab1b99a63c306caf8af5c3c12
Mick Carter <cartemic@oregonstate.edu>
2020-03-06 02:14:32 -0800

    Clean up some malarkey
    whoa jack back in my day we didn't butcher our own scripts

--- scripts/sensitivity_analysis.py
+++ scripts/sensitivity_analysis.py
@@ -22,115 +22,120 @@ import cantera as ct
 
 def study_with_progress_bar(
         progress_bar,
-        chanism,
-        itial_temp,
-        itial_press,
-        uivalence,
-        el,
-        idizer,
-        luent,
-        luent_mol_frac,
-        ert,
-        rturbation_fraction,
-        iii
+        mechanism,
+        initial_temp,
+        initial_press,
+        equivalence,
+        fuel,
+        oxidizer,
+        diluent,
+        diluent_mol_frac,
+        inert,
+        perturbation_fraction,
+        rxn_no,
+        lock
 ):
     perform_study(
-        chanism,
-        itial_temp,
-        itial_press,
-        uivalence,
-        el,
-        idizer,
-        luent,
-        luent_mol_frac,
-        ert,
-        rturbation_fraction,
-        iii
+        mechanism,
+        initial_temp,
+        initial_press,
+        equivalence,
+        fuel,
+        oxidizer,
+        diluent,
+        diluent_mol_frac,
+        inert,
+        perturbation_fraction,
+        rxn_no,
+        lock
     )
     progress_bar.update(1)
 
 
 if __name__ == '__main__':
     import warnings
-    warnings.simplefilter('ignore')
+    # warnings.simplefilter('ignore')
     # inert = 'AR'
-    inert = 'None'
-    # mechanism = 'Mevel2017.cti'
-    mechanism = 'gri30.cti'
-    inert_species = [inert]
-    initial_temp = 300
-    initial_press = 101325
-    equivalence = 1
-    fuel = 'CH4'
-    oxidizer = 'N2O'
+    _inert = 'None'
+    # _mechanism = 'Mevel2017.cti'
+    # _mechanism = 'aramco2.cti'
+    _inert_species = [_inert]
+    _mechanism = "gri30.cti"
+    _initial_temp = 300
+    _initial_press = 101325
+    _equivalence = 1
+    _fuel = 'C3H8'
+    _oxidizer = 'O2:1 N2:3.76'
     # diluent = 'AR'
-    diluent = 'None'
-    diluent_mol_frac = 0
-    perturbation_fraction = 1e-4
+    _diluent = 'None'
+    _diluent_mol_frac = 0
+    _perturbation_fraction = 1e-6
 
     t = db.Table(
         'sensitivity.sqlite',
         'data'
     )
     exist_check = t.fetch_test_rows(
-        mechanism=mechanism,
-        initial_temp=initial_temp,
-        initial_press=initial_press,
-        fuel=fuel,
-        oxidizer=oxidizer,
-        equivalence=equivalence,
-        diluent=diluent,
-        diluent_mol_frac=diluent_mol_frac,
-        inert=inert
+        mechanism=_mechanism,
+        initial_temp=_initial_temp,
+        initial_press=_initial_press,
+        fuel=_fuel,
+        oxidizer=_oxidizer,
+        equivalence=_equivalence,
+        diluent=_diluent,
+        diluent_mol_frac=_diluent_mol_frac,
+        inert=_inert
     )['rxn_table_id']
     # if len(exist_check) > 0:
     #     t.delete_test(exist_check[0])
 
     reactions = []
     # noinspection PyCallByClass,PyArgumentList
-    for rxn in ct.Reaction.listFromFile(mechanism):
+    for rxn in ct.Reaction.listFromFile(_mechanism):
         if not any([
             s in list(rxn.reactants) + list(rxn.products)
-            for s in inert_species
+            for s in _inert_species
         ]):
             reactions.append(rxn)
 
-    pbar = tqdm(total=len(reactions))
+    # pbar = tqdm(total=len(reactions))
 
-    lock = Lock()
-    # p = Pool(initializer=init, initargs=(lock,))
+    _lock = Lock()
+    # p = Pool(initializer=init, initargs=(_lock,))
     # p.starmap(
-    #     study_with_progress_bar,
+    #     perform_study,
     #     [
     #         [
-    #             pbar,
-    #             mechanism,
-    #             initial_temp,
-    #             initial_press,
-    #             equivalence,
-    #             fuel,
-    #             oxidizer,
-    #             diluent,
-    #             diluent_mol_frac,
-    #             inert,
-    #             perturbation_fraction,
+    #             # pbar,
+    #             _mechanism,
+    #             _initial_temp,
+    #             _initial_press,
+    #             _equivalence,
+    #             _fuel,
+    #             _oxidizer,
+    #             _diluent,
+    #             _diluent_mol_frac,
+    #             _inert,
+    #             _perturbation_fraction,
     #             i,
+    #             _lock
     #         ] for i in range(len(reactions))
     #     ]
     # )
-    for i in range(len(reactions)):
+    for i in range(5):# range(len(reactions)):
         perform_study(
-            mechanism,
-            initial_temp,
-            initial_press,
-            equivalence,
-            fuel,
-            oxidizer,
-            diluent,
-            diluent_mol_frac,
-            inert,
-            perturbation_fraction,
+            _mechanism,
+            _initial_temp,
+            _initial_press,
+            _equivalence,
+            _fuel,
+            _oxidizer,
+            _diluent,
+            _diluent_mol_frac,
+            _inert,
+            _perturbation_fraction,
             i,
-            lock
+            _lock
         )
-        pbar.update(1)
+        # pbar.update(1)
+    print("woo!")
