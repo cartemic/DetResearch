06aeb12b0456cc985d7ef2e97352eed053abea08
Mick Carter <cartemic@oregonstate.edu>
2020-12-09 06:55:12 -0800

    Rework radial intensity scan using skimage.transform.rotate
    Peak was weirdly shifted from zero. rotating the image is faster and
    easier than figuring out why and, most importantly, it works well.

--- funcs/spectral/analysis/image.py
+++ funcs/spectral/analysis/image.py
@@ -48,13 +48,12 @@ def get_center(psd):
     return max_loc % cols, max_loc // cols
 
 
-@jit
+@njit
 def fix(n):
-    if n < 0:
-        sign = -1
-    else:
-        sign = 1
-    return int(np.floor(abs(n)) * sign)
+    sign = np.ones_like(n)
+    sign[n < 0] = -1
+
+    return (np.floor(np.abs(n)) * sign).astype(np.int64)
 
 
 @njit
@@ -67,20 +66,27 @@ def get_angular_intensity(
     half_steps = n_steps // 2
     intensity = np.zeros(n_steps)
     img_h, img_w = psd.shape
-    buffer = np.zeros((img_w, img_w))
+    # buffer = np.zeros((img_w+1, img_w+1))
     win_size = (2 * window + 1) ** 2
     x_c, y_c = get_center(psd)
 
-    for idx_int in range(n_steps):
-        theta = np.pi / half_steps * (idx_int + 1 - half_steps)
-        cos_th = np.cos(theta)
-        sin_th = np.sin(theta)
-        x = x_c - radius * sin_th + 1
-        y = y_c + radius * cos_th + 1
-        m = fix(x)
-        n = fix(y)
+    indices = np.arange(n_steps)
+    thetas = np.pi / half_steps * (indices + 1 - half_steps)
+    cosines = np.cos(thetas)
+    sines = np.sin(thetas)
+    xs = x_c - radius * sines + 1
+    ys = y_c + radius * cosines + 1
+    ms = fix(xs)
+    ns = fix(ys)
+    buffer = np.zeros((ms.max()+2, ns.max()+2)) * np.NaN
 
-        if (0 <= m) & (m <= img_h) & (0 <= n) & (n <= img_w):
+    for idx_int in indices:
+        m = ms[idx_int]
+        n = ns[idx_int]
+        x = xs[idx_int]
+        y = ys[idx_int]
+
+        if (0 <= m) & (m < img_h) & (0 <= n) & (n < img_w):
             for i in (m, m+1):
                 for j in (n, n+1):
                     local_sum = 0
@@ -105,63 +111,82 @@ def get_angular_intensity(
     return 180 / half_steps * (np.arange(1, 1025) - half_steps), intensity
 
 
-@njit
+# @njit
+# def get_radial_intensity(
+#         psd,
+#         theta,
+#         window,
+#         half_steps=1024
+# ):
+#     n_steps = half_steps * 2 + 1  # mirror about 0
+#     img_h, img_w = psd.shape
+#     # buffer = np.zeros((img_w+1, img_w+1)) * np.NaN
+#     x_c, y_c = get_center(psd)
+#     win_size = (2 * window + 1) ** 2
+#
+#     intensity = np.zeros(n_steps)
+#     cos_theta = np.cos(theta * np.pi / 180)
+#     sin_theta = np.sin(theta * np.pi / 180)
+#
+#     radii = np.sqrt(
+#         np.sum(
+#             np.square(
+#                 np.array([img_h, img_w]) / 2
+#             )
+#         )
+#     ) / half_steps * np.arange(-half_steps, half_steps+1)
+#
+#     xs = x_c - radii * sin_theta + 1
+#     ys = y_c + radii * cos_theta + 1
+#     ms = fix(xs)
+#     ns = fix(ys)
+#     buffer = np.zeros((ms.max()+2, ns.max()+2)) * np.NaN
+#
+#     for idx_int in range(n_steps):
+#         x = xs[idx_int]
+#         y = ys[idx_int]
+#         m = ms[idx_int]
+#         n = ns[idx_int]
+#
+#         if (0 <= m) & (m < img_h) & (0 <= n) & (n < img_w):
+#             for i in (m, m+1):
+#                 for j in (n, n+1):
+#                     local_sum = 0
+#                     for k in range(-window, window + 1):
+#                         for l in range(-window, window + 1):
+#                             if (0 <= i + k) & \
+#                                     (i + k < img_h) & \
+#                                     (0 <= j + l) & \
+#                                     (j + l < img_w):
+#                                 local_sum += psd[i + k - 1, j + l - 1] / \
+#                                     win_size
+#
+#                     buffer[i, j] = local_sum
+#
+#             p = x - m
+#             q = y - n
+#             comp_p = 1 - p
+#             comp_q = 1 - q
+#             intensity[idx_int] = \
+#                 comp_p * comp_q * buffer[m, n] + \
+#                 p * comp_q * buffer[m + 1, n] + \
+#                 q * comp_p * buffer[m, n + 1] + \
+#                 p * q * buffer[m + 1, n + 1]
+#
+#     return radii, intensity
+from skimage.transform import rotate
 def get_radial_intensity(
         psd,
         theta,
         window,
         half_steps=1024
 ):
-    n_steps = half_steps * 2 + 1  # mirror about 0
-    img_h, img_w = psd.shape
-    buffer = np.zeros((img_w, img_w)) * np.NaN
-    x_c, y_c = get_center(psd)
-    win_size = (2 * window + 1) ** 2
-
-    intensity = np.zeros(n_steps)
-    cos_theta = np.cos(theta * np.pi / 180)
-    sin_theta = np.sin(theta * np.pi / 180)
-
-    radii = np.sqrt(
-        np.sum(
-            np.square(
-                np.array([img_h, img_w]) / 2
-            )
-        )
-    ) / half_steps * np.arange(-half_steps, half_steps+1)
-
-    for idx_int, radius in enumerate(radii):
-        x = x_c - radius * sin_theta + 1
-        y = y_c + radius * cos_theta + 1
-        m = fix(x)
-        n = fix(y)
-
-        if (0 <= m) & (m < img_h) & (0 <= n) & (n < img_w):
-            for i in (m, m+1):
-                for j in (n, n+1):
-                    local_sum = 0
-                    for k in range(-window, window + 1):
-                        for l in range(-window, window + 1):
-                            if (0 <= i + k) & \
-                                    (i + k < img_h) & \
-                                    (0 <= j + l) & \
-                                    (j + l < img_w):
-                                local_sum += psd[i + k - 1, j + l - 1] / \
-                                    win_size
-
-                    buffer[i, j] = local_sum
-
-            p = x - m
-            q = y - n
-            comp_p = 1 - p
-            comp_q = 1 - q
-            intensity[idx_int] = \
-                comp_p * comp_q * buffer[m, n] + \
-                p * comp_q * buffer[m + 1, n] + \
-                q * comp_p * buffer[m, n + 1] + \
-                p * q * buffer[m + 1, n + 1]
-
-    return radii, intensity
+    rot = rotate(psd, -theta)
+    xc, yc = get_center(rot)
+    return (
+        np.arange(rot.shape[1]) - xc,
+        rot[yc, :]
+    )
 
 
 def peaks_to_measurements(
